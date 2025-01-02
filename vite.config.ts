6cff3842903483941b584faad35918586eea2d1d
Rodrigo Anastacio <rodrigo.anastacio@dtidigital.com.br>
2025-01-02 13:47:45 -0300

    chore: update config

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), '');
+  const env = loadEnv(mode, process.cwd(), "");
   return {
     plugins: [react()],
     optimizeDeps: {
@@ -12,6 +12,9 @@ export default defineConfig(({ mode }) => {
     define: {
       __VITE_SUPABASE_URL__: JSON.stringify(env.VITE_SUPABASE_URL),
       __VITE_SUPABASE_ANON_KEY__: JSON.stringify(env.VITE_SUPABASE_ANON_KEY),
+      __VITE_CLERK_PUBLISHABLE_KEY__: JSON.stringify(
+        env.VITE_CLERK_PUBLISHABLE_KEY
+      ),
     },
   };
 });
